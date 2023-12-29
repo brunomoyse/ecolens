2d5190cae83c690ccd7d0845d694e6b0b9f31533
brunomoyse <bruno.moyse@nccn.fgov.be>
2023-12-29 13:23:30 +0100

    Select GeoPortal layer dynamically

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,39 +2,132 @@
 "use client";
 
 import MapComponent from "@/components/map-component";
-import { EyeOpenIcon, EyeClosedIcon } from '@radix-ui/react-icons';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {useMap} from "@/context/map-context";
+import { Eye, EyeOff } from 'lucide-react';
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
+import {createTileLayerFromUrl} from "@/lib/utils"
+
+import {geoPortalService} from "@/types"
+
+const geoPortalBaseUrl: string = 'https://geoservices.wallonie.be/arcgis/rest/services/';
+const getJsonResponse: string = '?f=json';
+
+const formatSubCategoryName = (fullName: string) => {
+    // Split the full name by '/'
+    const parts = fullName.split('/');
+    // Return the second part if it exists, otherwise return the full name
+    return parts.length >= 2 ? parts[1] : fullName;
+};
+
+const getCategories = async (): Promise<string[]> => {
+    const data = await fetch(geoPortalBaseUrl + getJsonResponse);
+    const json = await data.json()
+    return json.folders ? json.folders : [];
+};
+
+const getSubCategories = async (selectedCategory: string): Promise<geoPortalService[]> => {
+    const data = await fetch(geoPortalBaseUrl + selectedCategory + getJsonResponse);
+    const json = await data.json()
+    return json.folders ? json.services : [];
+}
 
 export default function Home() {
-    const { map } = useMap(); // Assuming you are using the MapContext
+    const { map } = useMap();
     const [isLayerVisible, setIsLayerVisible] = useState(true);
 
+    const [categories, setCategories] = useState<string[]>([]);
+    const [selectedCategory, setSelectedCategory] = useState<string|null>(null);
+    const [subCategories, setSubCategories] = useState<geoPortalService[]>([]);
+    const [selectedSubCategory, setSelectedSubCategory] = useState<string|null>(null);
+
+
+    const handleCategoryChange = (category: string) => {
+        setSelectedCategory(category);
+        setSelectedSubCategory(null);
+        getSubCategories(category).then(setSubCategories);
+    }
+
+    const handleSubCategoryChange = (subcategory: string) => {
+        setSelectedSubCategory(subcategory);
+        let arcGisUrl = geoPortalBaseUrl + selectedCategory + '/' + subcategory + '/' + 'MapServer'
+        const newTileLayer = createTileLayerFromUrl(arcGisUrl);
+        map?.addLayer(newTileLayer);
+    }
+
+    useEffect(() => {
+        getCategories().then(setCategories)
+    }, []);
+
     // Toggle function to show/hide layers
-    const toggleLayerVisibility = () => {
+    const toggleLayerVisibility = (title: string) => {
         if (!map) return;
         setIsLayerVisible(!isLayerVisible);
-        const geoJsonLayer = map.getLayers().getArray()
-            .find(layer => layer.get('title') === 'wallonia-pre');
-        if (geoJsonLayer) {
-            geoJsonLayer.setVisible(!isLayerVisible);
+        const layer = map.getLayers().getArray()
+            .find(layer => layer.get('title') === 'pae-charleroi');
+        if (layer) {
+            layer.setVisible(!isLayerVisible);
         }
     };
+
+    let listOfLayersTitle = ['pae-charleroi']
+
     return (
-    <main className="flex min-h-screen max-h-screen">
-        <aside className="w-1/4 bg-gray-200 p-4 shadow-lg">
-            <h2 className="text-2xl font-bold text-center my-4">Layers</h2>
-            <div className="space-y-4">
-                <div onClick={toggleLayerVisibility} className="flex items-center p-2 hover:bg-gray-300 rounded-lg cursor-pointer">
-                    <span className="ml-2">PRE</span>
-                    <button  className="ml-auto">
-                        {/* Toggle Icon */}
-                        {isLayerVisible ? <EyeOpenIcon /> : <EyeClosedIcon />}
-                    </button>
+        <main className="flex min-h-screen max-h-screen">
+            <aside className="w-1/4 bg-gray-200 p-4 shadow-lg">
+                <h2 className="text-2xl font-bold text-center my-4">Layers</h2>
+                {/* GeoPortal categories */}
+                <div className="my-2">
+                    <Select onValueChange={handleCategoryChange}>
+                        <SelectTrigger>
+                            <SelectValue placeholder="Ajouter une couche" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            {categories.map(category => (
+                                <SelectItem key={category} value={category}>
+                                    {category.replace(/_/g, ' ')}
+                                </SelectItem>
+                            ))}
+                        </SelectContent>
+                    </Select>
                 </div>
-            </div>
-        </aside>
-        <MapComponent />
-    </main>
-  )
+
+                {/* GeoPortal subcategories, visible only when a category is selected */}
+                {selectedCategory && (<Select onValueChange={handleSubCategoryChange}>
+                    <SelectTrigger>
+                        <SelectValue placeholder="Sélectionner une couche" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        {subCategories.map(subCategory => (
+                            <SelectItem key={subCategory.name} value={formatSubCategoryName(subCategory.name)}>
+                                {formatSubCategoryName(subCategory.name).replace(/_/g, ' ')}
+                            </SelectItem>
+                        ))}
+                    </SelectContent>
+                </Select>)}
+
+                {listOfLayersTitle.map((title) => (
+                    <div key={title} className="space-y-4 my-2">
+                        <div onClick={() => toggleLayerVisibility(title)}
+                             className="flex items-center p-2 hover:bg-gray-300 rounded-lg cursor-pointer">
+                            <span className="ml-2">{title}</span>
+                            <button className="ml-auto">
+                                {/* Toggle Icon */}
+                                {isLayerVisible ? <Eye /> : <EyeOff />}
+                            </button>
+                        </div>
+                    </div>
+                ))}
+            </aside>
+
+            <MapComponent/>
+        </main>
+    )
 }
---
+++ frontend/src/types/index.ts
@@ -0,0 +1,6 @@
+interface geoPortalService {
+    name: string,
+    type: string
+}
+
+export type { geoPortalService };
