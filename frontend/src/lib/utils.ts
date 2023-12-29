1b779113656be28b6af2c800b65b5ddd6d9515e0
brunomoyse <bruno.moyse@nccn.fgov.be>
2023-12-29 13:23:30 +0100

    Select GeoPortal layer dynamically

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,18 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+import TileLayer from 'ol/layer/Tile';
+import { TileArcGISRest } from 'ol/source';
+
+const createTileLayerFromUrl = (url: string) => {
+  return new TileLayer({
+    source: new TileArcGISRest({
+      url: url
+    })
+  });
+}
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export {createTileLayerFromUrl};
