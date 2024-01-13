3a1c762786d6d7cacf37a22456cb0290322d7e2d
Marc Ducobu <marc.ducobu@posteo.be>
2024-01-13 16:38:45 +0100

    Celery : use TIME_ZONE from django

--- django/ecolens/celery.py
+++ django/ecolens/celery.py
@@ -5,7 +5,7 @@ import os
 
 from celery import Celery
 from celery.schedules import crontab
-
+from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecolens.settings")
 
@@ -14,7 +14,7 @@ app = Celery("ecolens")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
-app.conf.timezone = 'UTC'
+app.conf.timezone = settings.TIME_ZONE
 
 @app.task(bind=True)
 def debug_task(self):
