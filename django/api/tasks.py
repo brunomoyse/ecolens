eccfee965b8f14feb7949488797c0cbce710486f
Marc Ducobu <marc.ducobu@nccn.fgov.be>
2024-01-26 12:22:02 +0100

    Updating layer using admin action

--- django/api/tasks.py
+++ django/api/tasks.py
@@ -1,30 +1,40 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from datetime import datetime
-from .models import InputSource
+from .models import InputSource, InputSourceUpdate
 import geopandas as gpd
 from sqlalchemy import create_engine
 from django.db import connections
 from urllib.request import urlretrieve
+import logging
 
-
-@shared_task(name = "print_msg_main")
-def print_message(message, *args, **kwargs):
-    print(f"Celery is working!! Message is {message}")
+logging.basicConfig(level=logging.INFO)
 
 @shared_task(name = "download json and save to db")
-def download_json_and_save_to_db(*args, **kwargs):
-    django_database_settings = connections['default'].settings_dict
-    db_connection_string = (
-        f"postgresql+psycopg2://{django_database_settings['USER']}:{django_database_settings['PASSWORD']}@"
-        f"{django_database_settings['HOST']}:{django_database_settings['PORT']}/{django_database_settings['NAME']}"
-    )
-    db_engine = create_engine(db_connection_string)
+def download_json_and_save_to_db(update_id):
+    try:
+        update = InputSourceUpdate.objects.get(id=update_id)
+        source = update.source
 
-    sources = InputSource.objects.all()
+        django_database_settings = connections['default'].settings_dict
+        db_connection_string = (
+            f"postgresql+psycopg2://{django_database_settings['USER']}:{django_database_settings['PASSWORD']}@"
+            f"{django_database_settings['HOST']}:{django_database_settings['PORT']}/{django_database_settings['NAME']}"
+        )
+        db_engine = create_engine(db_connection_string)
 
-    for source in sources:
         temp_file = urlretrieve(source.url)
         gdf = gpd.read_file(temp_file[0])
 
         gdf.to_postgis(source.table, db_engine, if_exists='replace', index=False, schema=source.schema)
+
+        update = InputSourceUpdate.objects.get(id=update_id)
+        update.status = update.Status.SUCCESS
+        update.end_time = datetime.now()
+        update.save()
+
+    except Exception as e:
+        update.status = update.Status.FAIL
+        update.end_time = datetime.now()
+        update.message = str(e)
+        update.save()
--- django/ecolens/settings.py
+++ django/ecolens/settings.py
@@ -140,3 +140,17 @@ GRAPHENE = {
 # Celery settings
 CELERY_BROKER_URL = "redis://redis:6379"
 CELERY_RESULT_BACKEND = "redis://redis:6379"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
