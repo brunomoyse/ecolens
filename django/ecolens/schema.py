d2944a31772ceaa0fbebb3447e35dded962c854f
Marc Ducobu <marc.ducobu@posteo.be>
2024-02-02 13:00:01 +0100

    Adding api point for enterprise

--- django/ecolens/schema.py
+++ django/ecolens/schema.py
@@ -1,7 +1,16 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from api.models import Layer, View
+from api.models import Layer, EntreprisesSpwSiegesExportation
+
+class EntreprisesSpwSiegesExportationType(DjangoObjectType):
+    class Meta:
+        model = EntreprisesSpwSiegesExportation
+        fields = (
+            "ndeg_du_siege_social",
+            "nom_du_siege_social",
+            "nom_du_siege_social",
+            "nom_du_siege_d_exploitation")
 
 class LayerType(DjangoObjectType):
     class Meta:
@@ -11,6 +20,18 @@ class LayerType(DjangoObjectType):
 class Query(graphene.ObjectType):
     all_layers = graphene.List(LayerType)
     layer_by_id = graphene.Field(LayerType, id=graphene.Int(required=True))
+    enterprises = graphene.List(EntreprisesSpwSiegesExportationType, first=graphene.Int(), skip=graphene.Int())
+
+    def resolve_enterprises(self, info, first=None, skip=None):
+        queryset = EntreprisesSpwSiegesExportation.objects.all()
+
+        if skip:
+            queryset = queryset[skip:]
+
+        if first:
+            queryset = queryset[:first]
+
+        return queryset
 
     def resolve_all_layers(root, info):
         # We can easily optimize query count in the resolve method
