f8af38c0d027e2c2f3fe54dcfc3349c1b3a72bb4
Marc Ducobu <marc.ducobu@posteo.be>
2024-02-22 19:58:22 +0100

    Refactoring using black

--- django/api/tasks.py
+++ django/api/tasks.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from datetime import datetime
-from .models import InputSource, InputSourceUpdate
+from .models import InputSourceUpdate
 import geopandas as gpd
 from sqlalchemy import create_engine
 from django.db import connections
@@ -10,13 +10,14 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-@shared_task(name = "download json and save to db")
+
+@shared_task(name="download json and save to db")
 def download_json_and_save_to_db(update_id):
     try:
         update = InputSourceUpdate.objects.get(id=update_id)
         source = update.source
 
-        django_database_settings = connections['default'].settings_dict
+        django_database_settings = connections["default"].settings_dict
         db_connection_string = (
             f"postgresql+psycopg2://{django_database_settings['USER']}:{django_database_settings['PASSWORD']}@"
             f"{django_database_settings['HOST']}:{django_database_settings['PORT']}/{django_database_settings['NAME']}"
@@ -26,7 +27,13 @@ def download_json_and_save_to_db(update_id):
         temp_file = urlretrieve(source.url)
         gdf = gpd.read_file(temp_file[0])
 
-        gdf.to_postgis(source.table, db_engine, if_exists='replace', index=False, schema=source.schema)
+        gdf.to_postgis(
+            source.table,
+            db_engine,
+            if_exists="replace",
+            index=False,
+            schema=source.schema,
+        )
 
         update = InputSourceUpdate.objects.get(id=update_id)
         update.status = update.Status.SUCCESS
