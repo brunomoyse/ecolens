3132525544e2e1014fdcfc52e2ddeb45c14ca7ff
Bruno Moyse <bruno.moyse@nccn.fgov.be>
2024-02-27 17:46:57 +0100

    Update detail for circle search

--- frontend/src/components/cards/CircleInfoCard.tsx
+++ frontend/src/components/cards/CircleInfoCard.tsx
@@ -4,7 +4,7 @@ import { Search } from "lucide-react";
 import {useDraggable} from "@/lib/utils";
 import { Input } from "@/components/ui/input"
 import {Label} from "@/components/ui/label";
-import {useEffect} from "react";
+import {Button} from "@/components/ui/button";
 
 
 export default function CircleInfoCard() {
@@ -21,13 +21,24 @@ export default function CircleInfoCard() {
                     <h2 className="text-xl font-bold text-center pl-1 mr-4 select-none">Recherche</h2>
                 </div>
             </header>
-            <div className="p-6">
-                <Label htmlFor="text">Rayon de recherche (km)</Label>
-                <Input type="number" value={(drawnCircle.getRadius()/1000).toFixed(2)} />
+            <div className="px-6 pb-6 pt-2">
+                <div className="flex pb-2">
+                    <div className="flex items-center w-2/3">
+                        <Label htmlFor="text" className="">Rayon de recherche (mètres)</Label>
+                    </div>
+                    <Input type="number" className="w-1/3" value={(drawnCircle.getRadius()).toFixed(0)} />
+                </div>
+
                 <ul className="list-disc space-y-2">
-                    <li>1</li>
-                    <li>2</li>
+                    <li><strong>PAE:</strong> 1</li>
+                    <li><strong>Entreprises:</strong> 1</li>
+                    <li><strong>Emplois:</strong> 1</li>
+                    <li><strong>Parcelles:</strong> 1</li>
+                    <li><strong>Parcelles inoccupées:</strong> 1</li>
                 </ul>
+
+                <Button size="sm" className="w-full mt-4 uppercase">Exporter détail</Button>
+
             </div>
         </aside>
     );
